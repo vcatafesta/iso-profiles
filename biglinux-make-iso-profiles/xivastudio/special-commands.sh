2b977a6c89362b08958d8924790b14d11440b1a2
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2023-04-13 19:15:46 -0300

    new xivastudio profile

--- biglinux-make-iso-profiles/xivastudio/special-commands.sh
+++ biglinux-make-iso-profiles/xivastudio/special-commands.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
     
 # Add some things from Packages-Desktop
-sed -n '/## Printing/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
-sed -n '/## Xorg Server and Graphics/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
-sed -n '/## Xorg Input Drivers/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
-sed -n '/## Misc/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
-sed -i 's|xf86-input-void||g'  biglinux/kde/Packages-Desktop
+# sed -n '/## Printing/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
+# sed -n '/## Xorg Server and Graphics/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
+# sed -n '/## Xorg Input Drivers/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
+# sed -n '/## Misc/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
+# sed -i 's|xf86-input-void||g'  biglinux/kde/Packages-Desktop
 
 sed -i '/GRUB_DISTRIBUTOR=/s/GRUB_DISTRIBUTOR=.*/GRUB_DISTRIBUTOR="XIVAStudio"/' biglinux/xivastudio/root-overlay/etc/default/grub
 sed -i '/User=/s/User=.*/User=xivastudio/' biglinux/xivastudio/root-overlay/etc/sddm.conf
@@ -19,11 +19,10 @@ sed -i '/hostname=/s/hostname=.*/hostname="xivastudio"/' biglinux/xivastudio/pro
 sed -i '/username=/s/username=.*/username="xivastudio"/' biglinux/xivastudio/profile.conf
 sed -i '/password=/s/password=.*/password="xivastudio"/' biglinux/xivastudio/profile.conf
 
+# sed -i '//s///' biglinux/xivastudio/
 
-# sed -i '//s///' biglinux/xivastudio/
-# sed -i '//s///' biglinux/xivastudio/
-# sed -i '//s///' biglinux/xivastudio/
-# sed -i '//s///' biglinux/xivastudio/
-# sed -i '//s///' biglinux/xivastudio/
-# sed -i '//s///' biglinux/xivastudio/
-# sed -i '//s///' biglinux/xivastudio/
+#add biglinux standed desktop to xivastudio desktop
+cat biglinux/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
+
+#remove desktop
+grep -v -f biglinux-make-iso-profiles/xivastudio/Desktop-remove  biglinux/xivastudio/Packages-Desktop  >  biglinux/xivastudio/Packages-Desktop
